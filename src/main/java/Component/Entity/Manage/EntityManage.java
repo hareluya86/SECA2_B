d3214d77eb770b7f53986c0a51191b630390ca94
hareluya86 <hareluya86@hotmail.com>
2014-03-06 00:48:37 +0800

    renamed:    src/main/java/Bootstrap/BootstrapDemo.java -> src/main/java/Bootstrap/Demo/BootstrapDemo.java 	new file:   src/main/java/Component/Entity/Manage/EntityManage.java 	modified:   src/main/webapp/programs/file/layout.xhtml 	new file:   src/main/webapp/programs/file/manage.xhtml 	modified:   src/main/webapp/programs/file/search.xhtml 	modified:   src/main/java/Bootstrap/Bootstrap.java 	modified:   src/main/java/Bootstrap/Demo/BootstrapDemo.java 	modified:   src/main/java/Bootstrap/Demo/ProgramFile.java 	modified:   src/main/java/Bootstrap/ProgramFactory.java

---
+++ src/main/java/Component/Entity/Manage/EntityManage.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Component.Entity.Manage;
+
+import EDS.BusinessUnit.EnterpriseUnit;
+import EDS.Data.DAO;
+
+/**
+ *
+ * @author KH
+ */
+public interface EntityManage<T extends EnterpriseUnit> {
+    
+    /**
+     * Returns the managed EnterpriseUnit object
+     * <p>
+     * I have no idea if this is going to be used yet...let's just keep it and
+     * see what happens.
+     * Oh! This is a facade class for the underlying EnterpriseUnit instance. When 
+     * you produce a screen that edits a particular EnterpriseUnit object, for 
+     * example, Customer1, you would not load the actual Customer1 instance on 
+     * the screen because you will need:
+     * <ul>
+     * <li>Validations - not advisable to implement in persistence class</li>
+     * <li>Conversions - Codes should be converted to descriptions.</li>
+     * 
+     * @return T
+     */
+    public T getEntity();
+    
+    /**
+     * Updates the underlying EnterpriseUnit object into the database
+     * <p>
+     * This method accepts an DAO instance and should call the update method of 
+     * the DAO and pass in its underlying EnterpriseUnit object instance.
+     * 
+     * @param dao The DAO instance used to flush the EnterpriseUnit into the database.
+     */
+    public void update(DAO dao);
+    
+    
+    
+}
