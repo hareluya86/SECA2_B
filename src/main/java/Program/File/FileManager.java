9a7db871f67c8b82dd961f44afac45db4379dd0e
hareluya86 <hareluya86@hotmail.com>
2014-03-20 10:28:35 +0800

    new file:   src/main/java/Program/File/FileManager.java

---
+++ src/main/java/Program/File/FileManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Program.File;
+
+import javax.inject.Named;
+
+/**
+ * Utility class that processes file and sequence objects
+ * <p>
+ * 
+ * @author vincent.a.lee
+ */
+@Named("FileManager")
+public class FileManager {
+    
+}
